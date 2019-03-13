ce45c65b50360abdbd7b5fc1f24605cadb7b4496
puzhu <hari.subhash@gmail.com>
2019-03-12 21:48:28 -0400

    updated class notes

---
+++ Classes/helpers.R
@@ -0,0 +1,10 @@
+pickRandom <- function() {
+    fullList <- c("Adiam Tesfaselassie", "Annette Fay", "Bobby Varghese Mammen", "Diana Ayala", "Emma Loebelson", "Erika Jauregui", "Fahima Islam", "Ian Hollands", "Jordan Willcox", "Marc Franzoni", "Marion Bacher", "Mehrnoosh", "Michael Humes", "Robert Richards", "Shannon Davis", "Vinoy Phillip", "Zeenat Nazir")
+    
+    attendees <- c("Adiam Tesfaselassie", "Annette Fay", "Bobby Varghese Mammen", "Diana Ayala", "Emma Loebelson", "Erika Jauregui", "Fahima Islam", "Ian Hollands", "Jordan Willcox", "Marc Franzoni", "Marion Bacher", "Mehrnoosh", "Michael Humes", "Robert Richards", "Shannon Davis", "Vinoy Phillip", "Zeenat Nazir")
+    
+    attendees[sample(1:20, 1)]
+}
+
+
+
